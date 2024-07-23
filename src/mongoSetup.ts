93a851cc6a49a0acaac91757e0110fcaca1b9b16
Pedro Sierra <pedroluis1280@gmail.com>
2024-07-23 10:07:52 -0500

    Add new start script

--- src/mongoSetup.ts
+++ src/mongoSetup.ts
@@ -5,7 +5,7 @@ const { MONGO_USER: user, MONGO_PWD: pwd } = process.env;
 (async () => {
     try {
         await mongoose.connect(`mongodb+srv://${user}:${pwd}@restdb.vf6ibgj.mongodb.net/?retryWrites=true&w=majority&appName=restdb`);
-        console.log('MongooDB connected');
+        console.log('MongoDB connected');
     } catch (error) {
         console.error(error);
     }
